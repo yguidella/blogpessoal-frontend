dfd0be456102f08302ef00fe30d0b93209e4efb3
yguidella <y.guidella8@outlook.com>
2026-05-12 21:38:46 -0300

    config: tailwind

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,69 +1,28 @@
 function Home() {
     return (
         <>
-            <div style={{
-                backgroundColor: "#6A1B9A",
-                display: "flex",
-                justifyContent: "center",
-                minHeight: "80vh"
-            }}>
-                <div style={{
-                    display: "grid",
-                    gridTemplateColumns: "1fr 1fr",
-                    color: "white",
-                    width: "100%",
-                    maxWidth: "1280px"
-                }}>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "1rem",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        paddingTop: "1rem",
-                        paddingBottom: "1rem"
-                    }}>
-                        <h2 style={{
-                            fontSize: "3rem",
-                            fontWeight: "bold"
-                        }}>
+            <div className="bg-indigo-900 flex justify-center min-h-[80vh]">
+                <div className="grid grid-cols-2 text-white w-full max-w-[1280px]">
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className="text-5xl font-bold text-center">
                             Sejam bem vindos e bem vindas!
                         </h2>
-                        <p style={{
-                            fontSize: "1.25rem"
-                        }}>
+                        <p className="text-xl">
                             Expresse aqui seus pensamentos e opiniões livremente.
                         </p>
 
-                        <div style={{
-                            display: "flex",
-                            justifyContent: "space-around",
-                            gap: "1rem"
-                        }}>
-                            <div style={{
-                                borderRadius: "0.5rem",
-                                color: "black",
-                                border: "2px solid black",
-                                backgroundColor: "#FF9800",
-                                padding: "0.5rem 1rem",
-                                cursor: "pointer"
-                            }}>
+                        <div className="flex justify-around gap-4">
+                            <div className="rounded-lg text-black border-2 border-black bg-orange-500 px-4 py-2 cursor-pointer font-bold">
                                 Nova Postagem
                             </div>
                         </div>
                     </div>
 
-                    <div style={{
-                        display: "flex",
-                        justifyContent: "center"
-                    }}>
+                    <div className="flex justify-center">
                         <img
                             src="https://ik.imagekit.io/yytwlza66/caquizinha_hero_semfundo.png"
                             alt="Imagem Página Home"
-                            style={{
-                                width: "90%",
-                                objectFit: "contain"
-                            }}
+                            className="w-[90%] object-contain"
                         />
                     </div>
                 </div>
