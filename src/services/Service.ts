ac6dc576a41481b81ebb33c0a23b7430199fdf29
yguidella <y.guidella8@outlook.com>
2026-05-15 18:56:33 -0300

    Deploy do Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: 'https://blogpessoal-zyr8.onrender.com'
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastroUsuario = async (url: string, dados: Object, setDados: Function) => {
